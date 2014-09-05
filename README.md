8cd039aaac516a5e24b3b11a1f25f809276df2b2
louis <luyi.wang@sta.samsung.com>
2014-09-05 13:44:52 -0700

    revise setup.sh

--- README.md
+++ README.md
@@ -4,11 +4,14 @@
 ```
 git clone https://github.com/wangluyi1982/dotfiles .vim
 
-cd .vim
+cd ~/.vim
 git submodule init
 git submodule update
 git submodule foreach git pull origin master (optional)
 
 for s in `git submodule  --quiet foreach 'echo $name'` ; do git config submodule.$s.ignore untracked ; done
-ln -s .vim/vimrc ~/.vimrc
+ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/louiswang.zsh-theme ~/.vim/oh-my-zsh/themes/
+ln -s ~/.vim/oh-my-zsh ~/.oh-my-zsh
+ln -s ~/.vim/zshrc ~/.zshrc
 ```
--- setup.sh
+++ setup.sh
@@ -6,8 +6,12 @@ git submodule update
 git submodule foreach git pull origin master
 
 for s in `git submodule  --quiet foreach 'echo $name'` ; do git config submodule.$s.ignore untracked ; done
-ln -s ~/.vim/vimrc ~/.vimrc
+if [ -f ~/.vimrc ] ; then mv ~/.vimrc ~/.vimrc_bak ; fi
+ln -s ~/.vim/vimrc ~/.vimrc 
+if [ -d ~/.oh-my-zsh/ ] ; then rm -fr ~/.oh-my-zsh ; fi
+if [ -f ~/.vim/oh-my-zsh/themes/louiswang.zsh-theme ] ; then rm -fr ~/.vim/oh-my-zsh/themes/louiswang.zsh-theme; fi
 ln -s ~/.vim/louiswang.zsh-theme ~/.vim/oh-my-zsh/themes/
 ln -s ~/.vim/oh-my-zsh/ ~/.oh-my-zsh
+if [ -f ~/.zshrc ] ; then mv ~/.zshrc ~/.zhsrc_bak ; fi
 ln -s ~/.vim/zshrc ~/.zshrc
 
