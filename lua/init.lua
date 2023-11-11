7c2845dea8fd760485709989d69bcb675355bb8c
Luyi Wang <wangluyi1982@gmail.com>
2023-11-11 10:57:16 -0800

    update the init

--- lua/init.lua
+++ lua/init.lua
@@ -108,3 +108,18 @@ cmp.setup ({
 })
 vim.o.keywordprg = ':help'
 require 'java'
+local function isempty(s)
+		return s == nil or s == ""
+    end
+
+local function use_if_defined(val, fallback)
+		return val ~= nil and val or fallback
+	end
+local conda_prefix = os.getenv("CONDA_PREFIX")
+if not isempty(conda_prefix) then
+    vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, conda_prefix .. "/bin/python")
+    vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, conda_prefix .. "/bin/python")
+else
+    vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, "python")
+    vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, "python3")
+end
